e36509f60f2b4178a3176ddf1255fe8ffc679149
Josh <jseibel@wisc.edu>
2020-02-19 15:56:11 -0600

    Finished Lesson 1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-/* Map of GeoJSON data from StatesFireData.geojson */
+// Map of GeoJSON data from StatesFireData.geojson */
+
 //declare map var in global scope
 var map;
+var minValue;
 
-//function to instantiate the Leaflet map
+//Step 1: function to instantiate the Leaflet map
 function createMap(){
     //create the map
     map = L.map('mapid', {
@@ -19,8 +21,87 @@ function createMap(){
     getData();
 };
 
-//function to retrieve the data and place it on the map
+function calcMinValue(data){
+     
+    //create empty array to store all data values
+    var allValues = [];
+    
+    //loop through each state
+    for(var state of data.features){
+         //loop through each year
+         for(var year = 2008; year <= 2018; year+=1){
+               //get acres burned for current year
+              var value = state.properties[String(year) + " Acres Burned"];
+              //add value to array
+              allValues.push(value);
+          }
+    }
+    
+    //get minimum value of our array
+    var minValue = Math.min(...allValues)
+    
+    minValue = 500;
+    return minValue;
+}
+
+//calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+    
+    //constant factor adjusts symbol sizes evenly
+    var minRadius = 1;
+    
+    //Flannery Appearance Compensation formula
+    var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
+
+    return radius;
+};
+
+// Add circle markers for point features to the map
+function createPropSymbols(data){
+    //create marker options
+    var geojsonMarkerOptions = {
+        radius: 8,
+        fillColor: "#990000",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+    // Determine which attribute to visualize with proportional symbols
+    var attribute = "2018 Acres Burned";
+
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+            //For each feature, determine its value for the selected attribute
+            var attValue = Number(feature.properties[attribute]);
+
+            //Give each feature's circle marker a radius based on its attribute value
+            geojsonMarkerOptions.radius = calcPropRadius(attValue);
+
+            //examine the attribute value to check that it is correct
+            //console.log(feature.properties, attValue);
+
+            //create circle markers
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        }
+    }).addTo(map);
+};
+
+//Import GeoJSON data
 function getData(){
+    //load the data
+    $.getJSON("data/StatesFireData.geojson", function(response){
+        //calculate minimum data value
+        minValue = calcMinValue(response);
+
+        //call function to create proportional symbols
+        createPropSymbols(response);
+    });
+};
+
+//function to retrieve the data and place it on the map
+function getDataOld(){
     //load the data
     $.getJSON("data/StatesFireData.geojson", function(response){
         //create marker options
@@ -45,8 +126,8 @@ function getData(){
         //create a Leaflet GeoJSON layer and add it to the map
         L.geoJson(response, {
             pointToLayer: function (feature, latlng){
-                //return L.circleMarker(latlng, geojsonMarkerOptions);
-                return L.marker(latlng, {icon: fireIcon});
+                return L.circleMarker(latlng, geojsonMarkerOptions);
+                //return L.marker(latlng, {icon: fireIcon});
             },
             onEachFeature: function onEachFeature(feature, layer) {
                 //no property named popupContent; instead, create html string with all properties
